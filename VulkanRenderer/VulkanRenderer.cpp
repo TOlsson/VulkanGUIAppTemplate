bcd80341e84b96496e47784433c07dad6dc449aa
Tobias <Tobias@TOBIAS-DATOR>
2023-01-28 12:00:15 +0100

    Add project files.

---
+++ VulkanRenderer/VulkanRenderer.cpp
@@ -0,0 +1,12 @@
+﻿// VulkanRenderer.cpp : Defines the entry point for the application.
+//
+
+#include "VulkanRenderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
